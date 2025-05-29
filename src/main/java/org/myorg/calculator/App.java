bf5b17c16d72d78c1273551e6cef8d1f99696254
manjuc21 <mac21cs@gmail.com>
2025-05-29 12:53:05 +0530

    dd

---
+++ src/main/java/org/myorg/calculator/App.java
@@ -0,0 +1,10 @@
+package org.myorg.calculator;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("2 + 3 = " + calc.add(2, 3));
+        // …etc…
+    }
+}
+
